6c59f1b18d4e6168d3b7203300e200ed9cba13f2
Christian <shadow2ube@users.noreply.github.com>
2022-05-21 11:35:22 -0400

    started adding login

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-//#define CPPHTTPLIB_OPENSSL_SUPPORT
+#define CPPHTTPLIB_OPENSSL_SUPPORT
 
 #include <iostream>
 #include <string>
@@ -30,12 +30,17 @@ using httplib::Server;
 using nlohmann::json;
 using namespace std::chrono;
 
-int64_t snowflake_gen(int wid, int pid, int seq) {
-//  int t = std::chrono::duration_cast<std::chrono::milliseconds>((_t - ).count();
-  return 1;
+uint64_t seq;
+
+int64_t snowflake_gen(uint64_t mid) {
+  uint64_t t = duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
+  uint64_t out = (t << 23) | ((mid << 52) >> 40) | ((seq << 52) >> 52);
+
+  seq++;
+  return out;
 }
 
-pqxx::result get_from_sql(const std::string &s) {
+pqxx::result get_from_sql(const std::string &s) noexcept {
   // unsafe lol
 //  try {
   SQL_CONN;
@@ -45,9 +50,61 @@ pqxx::result get_from_sql(const std::string &s) {
 //  }
 }
 
+std::string make_safe(std::string in) {
+  std::string::size_type pos;
+  while ((pos = in.find_first_of("\t\n\"\\", pos)) != std::string::npos) {
+    switch (in[pos]) {
+      case '\t':in.replace(pos, 1, "\\t");
+        pos += 2;
+        break;
+      case '\n':in.replace(pos, 1, "\\n");
+        pos += 2;
+        break;
+      case '\\':in.replace(pos, 1, "\\\\");
+        pos += 2;
+        break;
+      case '\"':in.replace(pos, 1, R"(\\\")");
+        pos += 2;
+        break;
+    }
+  }
+  return in;
+}
+
+char *sha256(const char *str, size_t length) {
+  int n;
+  SHA256_CTX c;
+  unsigned char digest[SHA256_DIGEST_LENGTH];
+  char *out = (char *) malloc(33);
+  SHA256_Init(&c);
+
+  while (length > 0) {
+    if (length > 512) SHA256_Update(&c, str, 512);
+    else SHA256_Update(&c, str, length);
+
+    length -= 512;
+    str += 512;
+  }
+  SHA256_Final(digest, &c);
+
+  for (n = 0; n < SHA256_DIGEST_LENGTH; ++n) snprintf(&(out[n * 2]), 16 * 2, "%02x", (unsigned int) digest[n]);
+//  return {out};
+}
+
+std::string remove_of(std::string in, std::string remove = "\t\n\"\\") {
+  for (auto i: remove) {
+    auto it = std::remove(in.begin(), in.end(), i);
+    auto r = std::distance(it, in.end());
+    in.erase(it, in.end());
+  }
+  return in;
+}
+
 int main() {
   Server svr;  //("./ignore/cert.pem", "./ignore/key.pem");
-  snowflake_gen(1, 2, 3);
+  snowflake_gen(22);
+
+  // region api requests
 
   svr.Get(R"(/api/usr/(\d+))", RES {
     auto id = req.matches[1];
@@ -102,9 +159,54 @@ int main() {
     res.set_content(j.dump(), "application/json");
   });
 
-  svr.Post("/api/login", RES {
-    res.set_header("a_token_4_u", "adfojlnferjkedrr3qiojur3oiuj3roijoji3fnlukwveoujihsjveokwlnvwrejlkn");
+  svr.Post("/api/login", [](
+      const httplib::Request &req,
+      httplib::Response &res,
+      const httplib::ContentReader &content_reader
+  ) {
+    if (req.is_multipart_form_data()) {
+      httplib::MultipartFormDataItems files;
+      content_reader([&](const httplib::MultipartFormData &file) {
+        files.push_back(file);
+        return true;
+      }, [&](const char *data, size_t data_length) {
+        files.back().content.append(data, data_length);
+        return true;
+      });
+      if (files.empty()) {
+        res.set_content("INVALID FORM INPUT", "text/plain");
+        return;
+      }
+      json j = json::object();
+      for (auto &i: files) {
+        j += {i.name, {
+            {"content", remove_of(i.content)},
+            {"content_type", remove_of(i.content_type)},
+            {"file_name", remove_of(i.filename)},
+        }};
+      }
+      std::stringstream ss;
+      std::cout << j["email"]["content"].get<std::string>() << std::endl;
+      try {
+        pqxx::result r = get_from_sql(
+            "SELECT passwd, salt FROM users WHERE email=\'"
+                + j["email"]["content"].get<std::string>()
+                + "\'");
+
+        std::string hashed;
+        sha256(&hashed, j["password"]["content"].get<std::string>() + std::to_string(r[0][1].as<int>()));
+        std::cout << hashed << std::endl;
+        if (r[0][0].as<std::string>() != hashed) throw std::exception();
+      } catch (const std::exception &e) {
+        res.set_content("INVALID EMAIL OR PASSWORD", "text/plain");
+        return;
+      }
+    } else {
+      res.set_content("INVALID FORM INPUT", "text/plain");
+    }
   });
+
+  //endregion
 
   svr.set_logger([](const auto &req, const auto &res) {
     std::cout << "req: " << req.body << "\t-\tres: " << res.body << std::endl;
