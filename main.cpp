e986caac1fd377169849434ef4d0c455294e479e
Christian <shadow2ube@users.noreply.github.com>
2022-05-22 01:26:51 -0400

    Imma go to bed

--- main.cpp
+++ main.cpp
@@ -222,7 +222,7 @@ int main() {
   });
   svr.set_post_routing_handler([](const auto &req, auto &res) {
     res.set_header("Access-Control-Allow-Origin", "*");
-    res.set_header("Access-Control-Allow-Headers", "Cookie");
+    res.set_header("Access-Control-Allow-Headers", "Cookie, no");
   });
   svr.set_read_timeout(5, 0); // 5 seconds
   svr.set_write_timeout(5, 0); // 5 seconds
