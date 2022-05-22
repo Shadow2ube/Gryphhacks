7b50940e1a97b2bff2ee4d5579e992b772357397
Christian <shadow2ube@users.noreply.github.com>
2022-05-21 22:56:07 -0400

    ALOT  - refactored and moved functions to separate files because it was getting outa hand  - added picosha2 library because the other methods of making sha256 sucked  - finally got sessions, session storage, authentification and authorization  - acquired more mental anguish that I wanted

--- main.cpp
+++ main.cpp
@@ -2,21 +2,17 @@
 
 #include <iostream>
 #include <string>
-#include <chrono>
+#include <pqxx/pqxx>
+#include <cstdio>
+#include <yaml-cpp/yaml.h>
+#include <yaml.h>
 #include "httplib.h"
 #include "json.hpp"
-#include <pqxx/pqxx>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <netdb.h>
-#include <arpa/inet.h>
+#include "util.h"
+#include "picosha2.h"
 
-#define RES [](const httplib::Request &req, httplib::Response &res)
-#define REQ [](const httplib::Request &req, httplib::Response &)
-#define SQL_URL "postgresql://hecker:HIh05t8y3KT7LJC7EtDmag@free-tier11.gcp-us-east1.cockroachlabs" \
-".cloud:26257/test?sslmode=verify-full&options=--cluster%3Dchill-whale-766"
-#define SQL_CONN pqxx::connection CONN(SQL_URL); pqxx::nontransaction conn(CONN)
+#define RES [&](const httplib::Request &req, httplib::Response &res)
+#define REQ [&](const httplib::Request &req, httplib::Response &)
 
 /*
  * TODO:
@@ -33,130 +29,54 @@
 using httplib::Server;
 using nlohmann::json;
 using namespace std::chrono;
+using namespace util;
 
-char *get_local_ip() {
-  char host[256];
-  int hostname = gethostname(host, sizeof(host));
-
-  if (hostname == -1) std::cout << "Error: Get Host Name" << std::endl;
-
-  struct hostent *host_entry;
-  host_entry = gethostbyname(host);
-
-  if (host_entry == nullptr) std::cout << "Error: Get Host Entry" << std::endl;
-
-  char *IP;
-  IP = inet_ntoa(*((struct in_addr *) host_entry->h_addr_list[0]));
-
-  return IP;
-
+std::string parse_url() {
+  std::ifstream in;
+  in.open("./settings.no");
+  std::string line;
+  getline(in, line);
+  return line;
 }
+bool valid_pass(const std::string &hash, const std::string &passwd, const std::string &salt);
 
-uint64_t seq; // yes this is weird but too bad
-uint64_t snowflake_gen(uint64_t mid) {
-  uint64_t t = duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
-  uint64_t out = (t << 23) | ((mid << 52) >> 40) | ((seq << 52) >> 52);
+enum perm_level {
+  NO_AUTH = 0,
+  USER = 1,
+  HOST = 2,
+  ADMIN = 3
+};
 
-  seq++;
-  return out;
-}
-
-std::string generate_uuid(std::string &seed) {
-  auto sum = std::accumulate(seed.begin(), seed.end(), 0);
-  std::mt19937 gen(sum);
-  std::uniform_int_distribution<> dis(0, 15);
-  std::uniform_int_distribution<> dis2(8, 11);
-
-  std::stringstream ss;
-  ss.clear();
-  int i;
-  ss << std::hex;
-  for (i = 0; i < 8; ++i) ss << dis(gen);
-  ss << "-";
-  for (i = 0; i < 4; ++i) ss << dis(gen);
-  ss << "-4";
-  for (i = 0; i < 3; ++i) ss << dis(gen);
-  ss << "-";
-  ss << dis2(gen);
-  for (i = 0; i < 12; ++i) ss << dis(gen);
-  std::cout << "uuid: " << ss.str() << std::endl;
-  return ss.str();
-}
-
-pqxx::result get_from_sql(const std::string &s) noexcept {
-  // unsafe lol
-//  try {
-  SQL_CONN;
-  return conn.exec(s);
-//  } catch (std::exception &e) {
-//    std::cerr << e.what() << std::endl;
-//  }
-}
-
-std::string make_safe(std::string in) {
-  std::string::size_type pos;
-  while ((pos = in.find_first_of("\t\n\"\\", pos)) != std::string::npos) {
-    switch (in[pos]) {
-      case '\t':in.replace(pos, 1, "\\t");
-        pos += 2;
-        break;
-      case '\n':in.replace(pos, 1, "\\n");
-        pos += 2;
-        break;
-      case '\\':in.replace(pos, 1, "\\\\");
-        pos += 2;
-        break;
-      case '\"':in.replace(pos, 1, R"(\\\")");
-        pos += 2;
-        break;
-    }
+perm_level get_perms(const std::string &url, const std::string &session_id) {
+  try {
+    pqxx::result r = get_from_sql(url,
+                                  "SELECT users.is_admin, users.is_host, sessions.uuid FROM users "
+                                  "INNER JOIN sessions ON sessions.user_id=users.id "
+                                  "WHERE sessions.uuid='" + session_id + "\'");
+    bool is_admin = r[0][0].as<bool>(), is_host = r[0][1].as<bool>();
+    if (is_admin) return perm_level::ADMIN;
+    if (is_host) return perm_level::HOST;
+  } catch (std::exception &ignored) {
+    return perm_level::NO_AUTH;
   }
-  return in;
-}
-
-char *sha256(const char *str, size_t length) {
-  int n;
-  SHA256_CTX c;
-  unsigned char digest[SHA256_DIGEST_LENGTH];
-  char *out = (char *) malloc(33);
-  SHA256_Init(&c);
-
-  while (length > 0) {
-    if (length > 512) SHA256_Update(&c, str, 512);
-    else SHA256_Update(&c, str, length);
-
-    length -= 512;
-    str += 512;
-  }
-  SHA256_Final(digest, &c);
-
-  for (n = 0; n < SHA256_DIGEST_LENGTH; ++n) snprintf(&(out[n * 2]), 16 * 2, "%02x", (unsigned int) digest[n]);
-  return out;
-}
-
-std::string remove_of(std::string in, std::string remove = "\t\n\"\\") {
-  for (auto i: remove) {
-    auto it = std::remove(in.begin(), in.end(), i);
-    auto r = std::distance(it, in.end());
-    in.erase(it, in.end());
-  }
-  return in;
+  return perm_level::USER;
 }
 
 int main() {
+  std::string sql_url = parse_url();
   get_local_ip();
 
   Server svr;  //("./ignore/cert.pem", "./ignore/key.pem");
-  snowflake_gen(22);
+  gen_snowflake(22);
 
   // region api requests
 
   svr.Get(R"(/api/usr/(\d+))", RES {
     auto id = req.matches[1];
-    pqxx::result r = get_from_sql("SELECT id, f_name, l_name, is_host from users where id=" + std::string(id));
+    pqxx::result r = get_from_sql(sql_url, "SELECT id, f_name, l_name, is_host from users where id=" + std::string
+        (id));
 
-    json j = json::object();
-    j += {
+    json j = {
         {"id", r[0][0].as<int>()},
         {"f_name", r[0][1].as<std::string>()},
         {"l_name", r[0][2].as<std::string>()},
@@ -166,7 +86,7 @@ int main() {
   });
 
   svr.Get("/api/events/loc", RES {
-    pqxx::result r = get_from_sql("SELECT id, name, lat, long FROM events");
+    pqxx::result r = get_from_sql(sql_url, "SELECT id, name, lat, long FROM events");
 
     json j = json::array();
     for (auto row: r) {
@@ -183,8 +103,9 @@ int main() {
   });
 
   svr.Get("/api/events/all", RES {
-    pqxx::result r = get_from_sql("SELECT id, user_id, min_age, is_recurring, name, description, long, lat, "
-                                  "time_start, time_end, location FROM events");
+    pqxx::result r = get_from_sql(sql_url, "SELECT id, user_id, min_age, is_recurring, name, description, long, "
+                                           "lat, "
+                                           "time_start, time_end, location FROM events");
 
     json j = json::array();
     for (auto row: r) {
@@ -205,51 +126,85 @@ int main() {
     res.set_content(j.dump(), "application/json");
   });
 
-  svr.Post("/api/login", [](
+  svr.Post("/api/login", [sql_url](
       const httplib::Request &req,
       httplib::Response &res,
       const httplib::ContentReader &content_reader
   ) {
     if (req.is_multipart_form_data()) {
-      httplib::MultipartFormDataItems files;
-      content_reader([&](const httplib::MultipartFormData &file) {
-        files.push_back(file);
-        return true;
-      }, [&](const char *data, size_t data_length) {
-        files.back().content.append(data, data_length);
-        return true;
-      });
-      if (files.empty()) {
-        res.set_content("INVALID FORM INPUT", "text/plain");
-        return;
-      }
-      json j = json::object();
-      for (auto &i: files) {
-        j += {i.name, {
-            {"content", remove_of(i.content)},
-            {"content_type", remove_of(i.content_type)},
-            {"file_name", remove_of(i.filename)},
-        }};
-      }
-      std::stringstream ss;
-      std::cout << j["email"]["content"].get<std::string>() << std::endl;
+      json j = read_multipart_form(req, res, content_reader);
       try {
         pqxx::result r = get_from_sql(
-            "SELECT passwd, salt, id FROM users WHERE email=\'"
-                + j["email"]["content"].get<std::string>()
-                + "\'");
-
-        std::string to_hash = j["password"]["content"].get<std::string>() + std::to_string(r[0][1].as<int>());
-        std::string hashed = std::string(sha256(to_hash.c_str(), to_hash.length()));
-        std::cout << hashed << std::endl;
-        if (r[0][0].as<std::string>() != hashed) throw std::exception();
-//        add_to_sql("INSERT INTO sessions (user_id, session_uuid, create_date)")
+            sql_url,
+            "SELECT salt, id, passwd FROM users WHERE email=\'"
+                + remove_of(j["email"]["content"])
+                + "\'"
+        );
+        auto id = r[0][1].as<std::string>(),
+            passwd_hash = r[0][2].as<std::string>(),
+            salt = r[0][0].as<std::string>();
+        if (!valid_pass(passwd_hash, j["password"]["content"].get<std::string>(), salt)) {
+          throw std::exception();
+        }
+        std::stringstream ss;
+        std::cout << "a" << std::endl;
+        ss << "INSERT INTO sessions (user_id) VALUES (" << id << ");";
+        std::cout << "b" << std::endl;
+        send_to_sql(sql_url, ss.str());
+        std::cout << "c" << std::endl;
+        std::cout << "d" << std::endl;
+        res.set_header("Cookie",
+                       "session="
+                           + get_from_sql(sql_url,
+                                          "SELECT uuid FROM sessions WHERE user_id=" + id)[0][0].as<std::string>()
+        );
+        std::cout << "e" << std::endl;
       } catch (const std::exception &e) {
-        res.set_content("INVALID EMAIL OR PASSWORD", "text/plain");
+        res.set_content("INVALID EMAIL OR PASSWORD, err: " + std::string(e.what()), "text/plain");
         return;
       }
     } else {
       res.set_content("INVALID FORM INPUT", "text/plain");
+    }
+  });
+
+  svr.Post("/api/events/create", [sql_url](
+      const httplib::Request &req,
+      httplib::Response &res,
+      const httplib::ContentReader &content_reader
+  ) {
+    if (get_perms(sql_url, req.get_header_value("Cookie").substr(5)) <= 1) {
+      res.set_content("Unauthorized", "text/plain");
+      return;
+    }
+    if (req.is_multipart_form_data()) {
+      json j = read_multipart_form(req, res, content_reader);
+      try {
+        std::stringstream ss;
+        ss << "INSERT INTO events (id, user_id, min_age, name, description, lat, long, time_start, time_end, "
+              "location) VALUES (" << (gen_snowflake(10) >> 1) << ","
+           //              << uid_from_session(sql_url, req.get_header_value("auth")) << ","
+           << "1234" << ","
+           << std::stoi(j["min_age"]["content"].get<std::string>()) << ",'"
+           << j["name"]["content"].get<std::string>() << "','"
+           << j["description"]["content"].get<std::string>() << "',"
+           << std::stof(j["lat"]["content"].get<std::string>()) << ","
+           << std::stof(j["long"]["content"].get<std::string>()) << ", TIMESTAMP '"
+           << j["time_start"]["content"].get<std::string>() << "', TIMESTAMP '"
+           << j["time_end"]["content"].get<std::string>() << "','"
+           << j["location"]["content"].get<std::string>() << "')";
+        send_to_sql(sql_url, ss.str());
+      } catch (const std::exception &e) {
+        res.set_content("invalid: " + std::string(e.what()), "text/plain");
+        return;
+      }
+    } else {
+      std::string body;
+      content_reader([&](const char *data, size_t data_length) {
+        body.append(data, data_length);
+        return true;
+      });
+      res.set_content("INVALID FORM INPUT: " + body, "text/plain");
     }
   });
 
@@ -258,7 +213,6 @@ int main() {
   svr.set_logger([](const auto &req, const auto &res) {
     std::cout << "req: " << req.body << "\t-\tres: " << res.body << std::endl;
   });
-
   svr.set_post_routing_handler([](const auto &req, auto &res) {
     res.set_header("Access-Control-Allow-Origin", "*");
   });
@@ -268,11 +222,8 @@ int main() {
   return 0;
 }
 
+bool valid_pass(const std::string &hash, const std::string &passwd, const std::string &salt) {
+  return hash == sha256(passwd + salt);
+}
 
-/*
- * 1. move the function elsewhere
- * 2. Christian commits and push
- * 3. Justin pulls
- * 4. Justin pastes function back
- * 5. Justin commits and push
- */
+
