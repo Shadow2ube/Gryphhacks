ae566004da993bb1fc9541ed74c5b83825b799b7
Christian <shadow2ube@users.noreply.github.com>
2022-05-22 00:50:51 -0400

    I want first place plz

--- main.cpp
+++ main.cpp
@@ -127,7 +127,7 @@ int main() {
   ) {
     try {
       json j = req.body;
-//      std::cout << j.dump() << std::endl;
+      std::cout << j.dump(2) << std::endl;
       pqxx::result r = get_from_sql(
           sql_url,
           "SELECT salt, id, passwd FROM users WHERE email=\'"
